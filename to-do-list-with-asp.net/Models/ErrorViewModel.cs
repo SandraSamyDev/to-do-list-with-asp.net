4a7b5452a87bf1c06d069ad8641494166b2f8840
Amal Zein <menam8656@gmail.com>
2026-04-24 18:55:32 +0300

    Initial ASP.NET MVC project with Bootstrap

---
+++ to-do-list-with-asp.net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace to_do_list_with_asp.net_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ to-do-list-with-asp.net/Program.cs
@@ -0,0 +1,36 @@
+namespace to_do_list_with_asp.net_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
