4a4859306d076601b6b5adad3a5b96658e5767e2
Fatima <fatmamahros93@gmail.com>
2026-04-30 11:29:23 -0400

    implement login and register page

---
+++ to-do-list-with-asp.net/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using to_do_list_with_asp.net_.Models;
+
+namespace to_do_list_with_asp.net_.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var savedEmail = HttpContext.Session.GetString("UserEmail");
+                var savedPassword = HttpContext.Session.GetString("UserPassword");
+
+                if (model.Email == savedEmail && model.Password == savedPassword)
+                {
+                    HttpContext.Session.SetString("IsLoggedIn", "true");
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Email or Password.");
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ to-do-list-with-asp.net/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace to_do_list_with_asp.net_.Models
+{
+    public class User
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
